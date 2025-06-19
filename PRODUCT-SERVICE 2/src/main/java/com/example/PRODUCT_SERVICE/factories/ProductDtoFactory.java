0180adc11c9e044181f907cf4e9d993351244440
aroslavvolkov <wartander135@gmail.com>
2025-06-19 14:41:45 +0400

    ProductDto

--- PRODUCT-SERVICE 2/src/main/java/com/example/PRODUCT_SERVICE/factories/ProductDtoFactory.java
+++ PRODUCT-SERVICE 2/src/main/java/com/example/PRODUCT_SERVICE/factories/ProductDtoFactory.java
@@ -1,8 +1,26 @@
 package com.example.PRODUCT_SERVICE.factories;
 
+import com.example.PRODUCT_SERVICE.dto.ProductDto;
+import com.example.PRODUCT_SERVICE.entity.Product;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductDtoFactory {
-    
+    public ProductDto createProductDto(Product product) {
+        ProductDto productDto = new ProductDto();
+        productDto.setTitle(product.getTitle());
+        productDto.setDescription(product.getDescription());
+        productDto.setPrice(product.getPrice());
+        productDto.setArticleNumber(product.getArticleNumber());
+        return productDto;
+    }
+
+    public Product createProduct(ProductDto productDto) {
+        Product product = new Product();
+        product.setTitle(productDto.getTitle());
+        product.setDescription(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+        product.setArticleNumber(productDto.getArticleNumber());
+        return product;
+    }
 }
